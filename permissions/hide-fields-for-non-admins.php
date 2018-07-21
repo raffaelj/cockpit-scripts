771de655ce37133aade3ee61dda2c34f4b0af6d0
Raffael <github@raffaeljesche.de>
2018-07-21 18:06:28 +0200

    added session/permissions warning

--- permissions/hide-fields-for-non-admins.php
+++ permissions/hide-fields-for-non-admins.php
@@ -8,6 +8,13 @@
     * edit collection and open "Permissions" tab
     * enable "read" permission and place the code below
   
+  Notes:
+  
+    Make sure, to start a new session before calling /cockpit/bootstrap.php
+    if you load the forbidden fields via PHP to your frontend. Otherwise you
+    could run into permission problems while logged in in one browser tab and
+    your frontend in another tab.
+  
 */
 
 $hiddenFields = ["slug"];
