25a47965590ebd906a9298c0f08dc00dede449d8
Raffael <github@raffaeljesche.de>
2018-09-17 15:11:52 +0200

    rewrote assets-path, much more robust, use the right trigger

--- full-paths-for-assets/bootstrap.php
+++ full-paths-for-assets/bootstrap.php
@@ -7,24 +7,15 @@
   
 */
 
-$app->on('collections.save.before.pages', function($name, &$entry, $isUpdate) use ($app) {
-
-    $asset_fields = [];
+$app->on('cockpit.assets.save', function(&$assets) use ($app) {
     
-    // get collection schema
-    $collection = $app->module('collections')->collection($name);
-    
-    // find asset fields
-    foreach ($collection['fields'] as $field) {
-        if ($field['type'] == 'asset')
-            $asset_fields[] = $field['name'];
-    }
-    
-    foreach ($asset_fields as $key => $val) {
+    foreach ($assets as &$asset) {
         
-        $entry[$val]['absolute_path'] = $app['site_url'] . $app['base_url'] . '/storage/uploads' . $entry[$val]['path'];
+        // add paths
+        $asset['absolute_path'] = $app['site_url'] . $app['base_url'] . '/storage/uploads' . $asset['path'];
         
-        $entry[$val]['real_path'] = COCKPIT_DIR . '/storage/uploads' . $entry[$val]['path'];
+        $asset['real_path'] = COCKPIT_DIR . '/storage/uploads' . $asset['path'];
+        
     }
     
 });
