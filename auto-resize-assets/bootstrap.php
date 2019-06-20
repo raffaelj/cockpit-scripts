b3581f025615e9084142daf324a3268b05f4d939
Raffael <github@raffaeljesche.de>
2019-06-20 12:54:07 +0200

    added snippet to resize assets automatically

---
+++ auto-resize-assets/bootstrap.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+  Resize assets to a maximum width after uploading and replace them with the resized ones
+
+  Copy this code snippet to `/config/bootstrap.php`.
+
+  You can set the max width in you config file with `image_max_width: 1024` or use
+  the default of 1920.
+
+  The upload process takes a while for large images, because
+
+  1. the original image is uploaded,
+  2. Colorthief picks the colors from the original file,
+  3. SimpleImage resizes the file,
+  4. and if you upload multiple images at once, steps 1-3 are multiplied
+
+*/
+
+$app->on('cockpit.assets.save', function(&$assets) {
+
+    $maxWidth  = $this->retrieve('image_max_width', 1920);
+
+    $method    = 'bestFit';
+    $quality   = 100;
+
+    foreach ($assets as &$asset) {
+
+        if (isset($asset['width']) && isset($asset['height']) && $asset['width'] > $width) {
+
+            $path = $this->path('#uploads:' . ltrim($asset['path'], '/'));
+
+            // resize image with `/lib/Lime/Helper/Image.php`, that calls claviska\SimpleImage
+            $img = $this('image')->take($path)->{$method}($width, $asset['height']);
+
+            $result = file_put_contents($path, $img->toString(null, $quality));
+
+            unset($img);
+
+            // don't overwrite meta, if write process failed
+            if ($result === false) continue;
+
+            $info = getimagesize($path);
+            $asset['width']  = $info[0];
+            $asset['height'] = $info[1];
+            $asset['size']   = filesize($path);
+
+        }
+
+    }
+
+});
